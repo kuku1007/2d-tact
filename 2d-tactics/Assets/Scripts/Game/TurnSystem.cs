79284837be6c2320e4a0317efa0fb14ff113389f
kuku1007 <lukas614@wp.pl>
2022-06-29 07:05:58 +0000

    turn system init

---
+++ 2d-tactics/Assets/Scripts/Game/TurnSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour
+{
+    public GameCH gameCH;
+    public int currentTurn = 1;
+    public string currentTeam; //new clss Team with units clss in umit class all stats, turn poimts etc it
+    // should have reference to charID
+    public string enemyTeam;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
